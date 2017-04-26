f591e4285905f22d01af6e87aba15c35dc90add2
Jae Hyun <jaehyunyim1997@gmail.com>
2017-04-25 20:48:14 -0700

    Added Divide and c conquer algo

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,6 +1,7 @@
 import math
 from decimal import Decimal
 
+#reads from input file and stores tuple points in array
 def readFromText():
 	points = []
 	f = open("input.txt")
@@ -25,8 +26,64 @@ def bruteForce(arr):
 				min = dist;
 	return min
 
+ #intex = 1 for sorted y, 0 for sorted x in tuple
+
+ #sorts array of tuples in x or y value
+ #intex = 1 for sorted y, 0 for sorted x in tuple
+ 
+def mergeSort(alist, index):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf, index)
+        mergeSort(righthalf, index)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i][index] < righthalf[j][index]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+
+def divide_and_conquer(arr): 
+	if len(arr) == 1:
+		return float('inf')
+	if len(arr) == 2:
+		return distanceFormula(arr[0], arr[1])
+
+	#splits into left and right subdivisions and sets min value to d
+	mid = len(arr)//2
+	lefthalf = arr[:mid]
+	righthalf = arr[mid:]
+	d = min(divide_and_conquer(lefthalf), divide_and_conquer(righthalf))
+
+	#checks if there is min value over the division line
+	newArr = [x for x in arr if x[0] >= mid - d and x[0] <= mid + d]
+	mergeSort(newArr, 1)
+	return min(bruteForce(newArr), d)
+
 arr = readFromText()
-distance = bruteForce(arr)
+
+# distance = bruteForce(arr)
+distance = divide_and_conquer(arr)
+
 text_file = open("input_distance.txt", "w")
 text_file.write(str(distance))
 text_file.close()
